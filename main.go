a44ba292484a61efca2d7bec5564958d4ca3c58d
Srikarrao1 <pasupuleti@antiersolutions.com>
2023-03-21 11:09:47 +0530

    first commit

--- main.go
+++ main.go
@@ -2,40 +2,54 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"sort"
+	
+
 )
 
 func main() {
-	houses := map[string][]string {
-		"gryffindor": {"weasley", "hagrid", "dumbledore", "lupin"},
-		"hufflepuf":  {"wenlock", "scamander", "helga", "diggory", "bobo"},
-		"ravenclaw":  {"flitwick", "bagnold", "wildsmith", "montmorency"},
-		"slytherin":  {"horace", "nigellus", "higgs", "bobo", "scorpius"},
-		"bobo":       {"wizardry", "unwanted"},
+	phones := map[string]string {
+		"bowen": "202-555-0179",
+		"dulin": "03.37.77.63.06",
+		"greco": "034989698793",
+	}
+	products := map[int]bool {
+		617841573: true,
+		879401371: false,
+		576872813: true,
 	}
 
-	delete(houses, "bobo")
-
-	args := os.Args[1:]
-	if len(args) < 1 {
-		fmt.Printf("Please provide a Hogwards house name\n")
-		return
+	multiPhones := map[string][]string {
+		"bowen": {"202-555-0179"},
+		"dulin": {"03.37.77.63.06", "03.37.70.50.05", "02.20.40.10.04"},
+		"greco": {"03489940240", "03489900120"},
+	}
+	basket := map[int]map[int]int {
+		100: {617841573: 4, 576872813: 2},
+		101: {576872813: 5, 657473833: 20},
+		102: {},
 	}
 
-	house, students := args[0], houses[args[0]]
+	who, phone := "dulin", "N/A"
 
-	if students == nil {
-		fmt.Printf("Sorry, I don't know anything about %q.\n", house)
-		return
+	if v, ok := phones[who]; ok {
+		phone = v
 	}
-	clone := append([]string(nil), students...)
-	sort.Strings(clone)
+	fmt.Printf("%s's phone number is %s\n", who, phone)
 
+	id, status := 879401371, "Available"
 
-	fmt.Printf("~~~ %s students ~~~\n\n", house)
-
-	for _, student := range clone {
-		fmt.Printf("\t+%s\n", student)
+	if !products[id] {
+		status = "Not" + status
 	}
+	fmt.Printf("Product ID #%d is %s\n", id, status)
+
+	who, phone = "greco", "N/A"
+
+	if phones := multiPhones[who]; len(phones) >= 2 {
+		phone = phones[1]
+	}
+	fmt.Printf("%s's second phone number is %s", who, phone )
+
+	cid, pid := 101, 576872813
+	fmt.Printf("Customer #%d is going to buy %d from product ID #%d.\n", cid, basket[cid][pid], pid)
 }
